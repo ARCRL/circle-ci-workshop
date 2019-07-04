b9191ca7a1aa2840b45b23319bb86da738ffe872
git <crlauridsen@live.dk>
2019-07-04 11:36:45 +0200

    QualityUpdate for conjured

--- src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
+++ src/main/java/net/praqma/codeacademy/gildedrose/GildedRose.java
@@ -12,8 +12,10 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros") && !items[i].name.equals("Sulfuras, Hand of Ragnaros")){
                         items[i].quality = items[i].quality - 1;
+                    } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                        items[i].quality = items[i].quality - 2;
                     }
                 }
             } else {
